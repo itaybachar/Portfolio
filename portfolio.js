1f05a9f2d4fa28edf770e1bd9a79047fb6d1bad9
itaybachar <itay.bachar01@gmail.com>
2018-11-28 14:09:32 -0500

    stuff

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,15 @@
-var expandButton = document.querySelector("button");
+var button = document.getElementById("snip1");
 var expanded = false;
-var theDiv = document.getElementById("project");
-var content = document.createElement('p');
-content += "AHRIUEHE IAUDNIWAND IUANDIWUA NDUAINW UWIAN DUIWAND IUWANDUI NAWDUIN AWIUDN IWUAND IUAWNDUIANDIUANDIUADNIUAWNDAIWUDNWAIUDN N AI NIW ND IAUND I awd";
-expandButton.addEventListener("click",function(){
-    if(!expanded){
+var theDiv = document.getElementById("code");
 
-        theDiv.appendChild(content);
+var content = "<p class='info' >AHRIUEHE IAUDNIWAND IUANDIWUA NDUAINW UWIAN DUIWAND IUWANDUI NAWDUIN AWIUDN IWUAND IUAWNDUIANDIUANDIUADNIUAWNDAIWUDNWAIUDN N AI NIW ND IAUND I awd</p>";
+button.addEventListener("click",function(){
+    if(!expanded){
+       // document.getElementById("code").innerHTML = "Argg";
+         theDiv.innerHTML = content;
+         expanded = true;
+    } else {
+        theDiv.innerHTML = "";
+        expanded = false;
     }
 });
